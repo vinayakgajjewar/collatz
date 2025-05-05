328e97854e4ba1ec2ec1fa28eb879b83e7611848
Vinayak Gajjewar <vinayakgajjewar@gmail.com>
2025-05-04 17:04:25 -0700

    uncle bob would be proud

--- collatz.cpp
+++ collatz.cpp
@@ -2,20 +2,42 @@
 #include <future>
 #include <iostream>
 
+const std::string DELIMITER = "->";
+
+bool is_even(unsigned long input) {
+    bool is_even;
+    if (input % 2 == 0) {
+        is_even = true;
+    } else {
+        is_even = false;
+    }
+    return is_even;
+}
+
+unsigned long collatz_iteration(unsigned long input) {
+    unsigned long output;
+    if (is_even(input)) {
+        output = input / 2;
+    } else {
+        output = (input * 3) + 1;
+    }
+    return output;
+}
+
+void print_orbit(unsigned long start) {
+    std::cout << start;
+    unsigned long current = start;
+    while (current != 1) {
+        current = collatz_iteration(current);
+        std::cout << DELIMITER << current;
+    }
+    std::cout << std::endl;
+}
+
 int main() {
     unsigned long current_start = 1;
     while (true) {
-        unsigned long current = current_start;
-        std::cout << current_start;
-        while (current != 1) {
-            if (current % 2 == 0) {
-                current /= 2;
-            } else {
-                current = (3 * current) + 1;
-            }
-            std::cout << "  -> " << current;
-        }
-        std::cout << std::endl;
+        print_orbit(current_start);
         current_start++;
         std::this_thread::sleep_for(std::chrono::seconds(1));
     }
